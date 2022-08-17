6e12f0ac2c20328f18c63b0145c23d171ad19d9e
linwencong <wencong.lin@airgram.io>
2022-08-17 14:26:02 +0800

    chore: fetch icons

--- src/icons/public-check-outlined.js
+++ src/icons/public-check-outlined.js
@@ -12,40 +12,11 @@ const PublicCheckOutlined = props => {
       fill={color}
       {...otherProps}
     >
-      <g clipPath="url(#clip0_298_3)">
-        <rect x=".01" y=".01" width="23.98" height="23.98"></rect>
-        <path d="M0 0l24 24"></path>
-        <path d="M24 0L0 24"></path>
-        <path d="M12 0v24"></path>
-        <path d="M24 12H0"></path>
-        <path d="M24 8H0"></path>
-        <path d="M24 16H0"></path>
-        <path d="M12 24V0"></path>
-        <path d="M16 24V0"></path>
-        <path d="M8 24V0"></path>
-        <rect x="4.01" y="2.01" width="15.98" height="19.98" rx=".99"></rect>
-        <rect
-          x="21.99"
-          y="4.01"
-          width="15.98"
-          height="19.98"
-          rx=".99"
-          transform="rotate(90 21.99 4.01)"
-        ></rect>
-        <rect x="3.01" y="3.01" width="17.98" height="17.98" rx=".99"></rect>
-        <circle cx="12" cy="12" r="9.99"></circle>
-        <circle cx="12" cy="12" r="4.99"></circle>
-      </g>
       <path
         fillRule="evenodd"
         clipRule="evenodd"
         d="M21.65 4.24a1 1 0 01.11 1.41l-12 14a1 1 0 01-1.467.057l-6-6a1 1 0 111.414-1.414l5.237 5.236L20.24 4.35a1 1 0 011.41-.108z"
       ></path>
-      <defs>
-        <clipPath id="clip0_298_3">
-          <rect width="24" height="24"></rect>
-        </clipPath>
-      </defs>
     </svg>
   );
 };
--- src/icons/public-close.js
+++ src/icons/public-close.js
@@ -12,40 +12,11 @@ const PublicClose = props => {
       fill={color}
       {...otherProps}
     >
-      <g clipPath="url(#clip0_89_8)">
-        <rect x=".01" y=".01" width="23.98" height="23.98"></rect>
-        <path d="M0 0l24 24"></path>
-        <path d="M24 0L0 24"></path>
-        <path d="M12 0v24"></path>
-        <path d="M24 12H0"></path>
-        <path d="M24 8H0"></path>
-        <path d="M24 16H0"></path>
-        <path d="M12 24V0"></path>
-        <path d="M16 24V0"></path>
-        <path d="M8 24V0"></path>
-        <rect x="4.01" y="2.01" width="15.98" height="19.98" rx=".99"></rect>
-        <rect
-          x="21.99"
-          y="4.01"
-          width="15.98"
-          height="19.98"
-          rx=".99"
-          transform="rotate(90 21.99 4.01)"
-        ></rect>
-        <rect x="3.01" y="3.01" width="17.98" height="17.98" rx=".99"></rect>
-        <circle cx="12" cy="12" r="9.99"></circle>
-        <circle cx="12" cy="12" r="4.99"></circle>
-      </g>
       <path
         fillRule="evenodd"
         clipRule="evenodd"
         d="M6.707 5.293a1 1 0 00-1.414 1.414L10.586 12l-5.293 5.293a1 1 0 101.414 1.414L12 13.414l5.293 5.293a1 1 0 001.414-1.414L13.414 12l5.293-5.293a1 1 0 00-1.414-1.414L12 10.586 6.707 5.293z"
       ></path>
-      <defs>
-        <clipPath id="clip0_89_8">
-          <rect width="24" height="24"></rect>
-        </clipPath>
-      </defs>
     </svg>
   );
 };
